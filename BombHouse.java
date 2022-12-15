96083849fe7fcace429ae9cc649d1a3df74c4415
Jami31 <100648398+Jami31@users.noreply.github.com>
2022-12-15 22:14:23 +0800

    firstcommit

--- BombHouse.java
+++ BombHouse.java
@@ -13,9 +13,10 @@ public class BombHouse implements ActionListener{
 	JLabel textfield = new JLabel();
 	JButton[] buttons = new JButton[25];
     JButton resetButton;
+	JButton startButton;
 	boolean player1_turn;
-    int bomb1 = (int)(Math.random() * 14) + 0;
-    int bomb2 = (int)(Math.random() * 25) + 14;
+    int bomb1; 
+    int bomb2;
 
 	BombHouse(){
 		
@@ -24,6 +25,7 @@ public class BombHouse implements ActionListener{
 		frame.getContentPane().setBackground(new Color(50,50,50));
 		frame.setLayout(new BorderLayout());
 		frame.setVisible(true);
+		frame.setResizable (false);
 		
 		textfield.setBackground(new Color(25,25,25));
 		textfield.setForeground(new Color(255,255,255));
@@ -38,10 +40,18 @@ public class BombHouse implements ActionListener{
         resetButton = new JButton();
         resetButton.setText("Restart");
         resetButton.setSize(100,50);
-        resetButton.setLocation(0, 18);
+        resetButton.setLocation(20, 17);
         resetButton.addActionListener(this);
         title_panel.add(resetButton);
         resetButton.setVisible(false);
+
+		startButton = new JButton();
+		startButton.setText("Start");
+        startButton.setSize(100,50);
+        startButton.setLocation(20, 17);
+        startButton.addActionListener(this);
+        title_panel.add(startButton);
+        startButton.setVisible(true);
 
 		
 		button_panel.setLayout(new GridLayout(5,5));
@@ -53,19 +63,43 @@ public class BombHouse implements ActionListener{
 			buttons[i].setFont(new Font("Trebuchet MS",Font.BOLD,40));
 			buttons[i].setFocusable(false);
 			buttons[i].addActionListener(this);
+			buttons[i].setEnabled(false);
+			buttons[i].setBackground(Color.BLACK);
 		}
+
+	
+
 		
 		title_panel.add(textfield);
 		frame.add(title_panel,BorderLayout.NORTH);
 		frame.add(button_panel);
 		
-		firstTurn();
+		buttons[0].setBackground(Color.BLUE);
+		buttons[4].setBackground(Color.BLUE);
+		buttons[5].setBackground(Color.BLUE);
+		buttons[6].setBackground(Color.BLUE);
+		buttons[8].setBackground(Color.BLUE);
+		buttons[9].setBackground(Color.BLUE);
+		buttons[10].setBackground(Color.BLUE);
+		buttons[12].setBackground(Color.BLUE);
+		buttons[14].setBackground(Color.BLUE);
+		buttons[15].setBackground(Color.BLUE);
+		buttons[19].setBackground(Color.BLUE);
+		buttons[20].setBackground(Color.BLUE);
+		buttons[24].setBackground(Color.BLUE);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
-		
+		if(e.getSource()==resetButton){
+			firstTurn();
+		}
+
+		if(e.getSource()==startButton){
+			startButton.setVisible(false);
+			firstTurn();
+		}
 
 		for(int i=0;i<25;i++) {
 			if(e.getSource()==buttons[i]) {
@@ -113,14 +147,27 @@ public class BombHouse implements ActionListener{
 	}
 	
 	public void firstTurn() {
+
+		for(int i=0;i<25;i++) {
+			buttons[i].setEnabled(true);
+			buttons[i].setBackground(Color.BLACK);
+			buttons[i].setText("");
+			
+		}
+
+		bomb1 = (int)(Math.random() * 25) + 0;
+		bomb2 = (int)(Math.random() * 25) + 0;
+
 		
 		if(random.nextInt(2)==0) {
 			player1_turn=true;
 			textfield.setText("Red turn");
+			textfield.setBackground(Color.RED);
 		}
 		else {
 			player1_turn=false;
 			textfield.setText("Blue turn");
+			textfield.setBackground(Color.BLUE);
 		}
 
 
